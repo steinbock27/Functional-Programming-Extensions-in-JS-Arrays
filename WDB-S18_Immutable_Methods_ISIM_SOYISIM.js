a1e853c6c9e49e27ee1c5663e7820648e18bb195
steinbock27 <ismetferdi@gmail.com>
2019-03-14 22:36:21 +0100

    second question is solved with the help of admins :(

--- WDB-S18_Immutable_Methods_ISIM_SOYISIM.js
+++ WDB-S18_Immutable_Methods_ISIM_SOYISIM.js
@@ -1,14 +1,19 @@
-//Submitted by: Ismet Ferdi SEN
-let cars = ["audi", "bmw", "ford", "opel", "fiat", "honda", "toyota", "renault", "tesla", "volvo"];
+  //Submitted by: Ismet Ferdi SEN
+  let cars = ["audi", "bmw", "ford", "opel", "fiat", "honda", "toyota", "renault", "tesla", "volvo"];
 
-//1) Icinde "o" harfi gecen arabalari ve bu arabalarin sayisini bulun.
+  //1) Icinde "o" harfi gecen arabalari ve bu arabalarin sayisini bulun.
 
-//ES6 Solution
-//icinde "o" harfi gecen arabalar
-let filter1= cars.filter(str=>{
-  return str.includes("o");
-});
-console.log(`icinde "o" harfi gecen arabalar: ${filter1}
-ve bu arabarin sayisi: ${filter1.length}`);
+  //ES6 Solution
+  //icinde "o" harfi gecen arabalar
+  let filter1= cars.filter(str=>{
+    return str.includes("o");
+  });
+  console.log(`icinde "o" harfi gecen arabalar: ${filter1}
+  ve bu arabarin sayisi: ${filter1.length}`);
 
-//
+  //2) Icinde iki defa "o" harfi gecen arabalarin sayisini bulun.
+    let filter2= cars.filter(str=>{
+      return Array.from(str)
+        .filter(araba => araba === "o").length === 2;
+      });
+      console.log(`Icinde iki defa "o" harfi gecen arabalar "${filter2}" ve sayisi: ${filter2.length}`);
